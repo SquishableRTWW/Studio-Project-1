1174cb16909a1f17892a8916bb111ef7a141b3cc
Cedric <yeojunwen@gmail.com>
2021-08-20 13:53:42 +0800

    Revert "this is my take on adding the monsters im still not sure if it will work yet so i got to do more testing"
    This reverts commit 78062d03246cd265a359b2ae05f3c3e1e90d6187.

--- SP1Framework/hunter.cpp
+++ SP1Framework/hunter.cpp
@@ -3,10 +3,7 @@
 hunter::hunter(void)
 {
 	string name = "null";
-	for (int i = 0; i < 6; i++)
-	{
-		party[i].setOGStats();
-	}
+	monster party[6];
 }
 
 void hunter::setname(string n)
@@ -17,36 +14,4 @@ void hunter::setname(string n)
 string hunter::getname()
 {
 	return name;
-}
-
-monster hunter::choosestarter(int s)
-{
-	if (s == 1)
-	{
-		party[0] = ignis();
-	}
-	else if (s == 2)
-	{
-		party[0] = typhis();
-	}
-	else if (s == 3)
-	{
-		party[0] = vitalus();
-	}
-}
-
-void hunter::addparty(int m)
-{
-	for (int i = 1; i < 6; i++)
-	{
-		if (m == 1)
-		{
-			party[i] = oris();
-		}
-	}
-}
-
-hunter::~hunter(void)
-{
-
 }
--- SP1Framework/hunter.h
+++ SP1Framework/hunter.h
@@ -1,10 +1,6 @@
 #pragma once
 #include <string>
 #include "monster.h"
-#include "ignis.h"
-#include "typhis.h"
-#include "vitalus.h"
-#include "oris.h"
 
 class hunter
 {
@@ -15,7 +11,4 @@ public:
 	hunter(void);
 	void setname(string n);
 	string getname();
-	monster choosestarter(int s);
-	void addparty(int m);
-	~hunter(void);
 };
