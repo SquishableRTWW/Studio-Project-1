94672e0e7591567285fdfbcd5a6be3a7dd3b0064
Cedric <yeojunwen@gmail.com>
2021-08-20 13:03:37 +0800

    stuff for the hunter

---
+++ SP1Framework/hunter.cpp
@@ -0,0 +1,17 @@
+#include "hunter.h"
+
+hunter::hunter(void)
+{
+	string name = "null";
+	monster party[6];
+}
+
+void hunter::setname(string n)
+{
+	name = n;
+}
+
+string hunter::getname()
+{
+	return name;
+}
