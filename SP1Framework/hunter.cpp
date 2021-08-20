78062d03246cd265a359b2ae05f3c3e1e90d6187
Cedric <yeojunwen@gmail.com>
2021-08-20 13:46:13 +0800

    this is my take on adding the monsters im still not sure if it will work yet so i got to do more testing

--- SP1Framework/hunter.cpp
+++ SP1Framework/hunter.cpp
@@ -3,7 +3,10 @@
 hunter::hunter(void)
 {
 	string name = "null";
-	monster party[6];
+	for (int i = 0; i < 6; i++)
+	{
+		party[i].setOGStats();
+	}
 }
 
 void hunter::setname(string n)
@@ -14,4 +17,36 @@ void hunter::setname(string n)
 string hunter::getname()
 {
 	return name;
+}
+
+monster hunter::choosestarter(int s)
+{
+	if (s == 1)
+	{
+		party[0] = ignis();
+	}
+	else if (s == 2)
+	{
+		party[0] = typhis();
+	}
+	else if (s == 3)
+	{
+		party[0] = vitalus();
+	}
+}
+
+void hunter::addparty(int m)
+{
+	for (int i = 1; i < 6; i++)
+	{
+		if (m == 1)
+		{
+			party[i] = oris();
+		}
+	}
+}
+
+hunter::~hunter(void)
+{
+
 }
